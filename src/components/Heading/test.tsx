7e7d9681882eec67b5f80b3288c7d48c710c9abf
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-10-07 14:05:19 -0300

    matcg media com teste quebrado

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Heading from '.'
 
@@ -9,5 +10,30 @@ describe('<Heading />', () => {
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       color: '#FAFAFA'
     })
+  })
+
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a heading line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+
+  it('should render a heading line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Won games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
   })
 })
