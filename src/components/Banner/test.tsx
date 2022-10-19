080f15fd764206ac1ee60e388b5095c0fb8cfbbc
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-10-19 15:05:47 -0300

    banner

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Banner
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /play the new Crashlands season/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
