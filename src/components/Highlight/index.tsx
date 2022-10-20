e58c20d63d392f9860b5afa07dad10c99506a5c4
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-10-20 17:25:33 -0300

    highlight

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,8 +1,32 @@
+import Button from 'components/Button'
 import * as S from './styles'
 
-const Highlight = () => (
-  <S.Wrapper>
-    <h1>Highlight</h1>
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+  backgroundImage: string
+  floatImage?: string
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+  backgroundImage,
+  floatImage
+}: HighlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
   </S.Wrapper>
 )
 
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,15 +1,37 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
 
+const props = {
+  title: 'heading 1',
+  subtitle: 'heading 2',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
+
 describe('<Highlight />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Highlight />)
+    renderWithTheme(<Highlight {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /Highlight/i })
+      screen.getByRole('heading', { name: /heading 1/i })
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
   })
 })
