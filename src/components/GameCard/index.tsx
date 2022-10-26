37194ccdecc803c1a4c86fca79c0b928f38c1467
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-10-26 17:19:11 -0300

    inicio gamecard

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { FavoriteBorder } from 'styled-icons/material-outlined'
+import * as S from './styles'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+const GameCard = ({ title, developer, img, price }: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <Image src={img} alt={title} />
+    </S.ImageBox>
+    <S.Info>
+      <S.Title>{title}</S.Title>
+      <S.Developer>{developer}</S.Developer>
+    </S.Info>
+    <S.FavButton role="button">
+      <FavoriteBorder aria-label="Add to wishlist" />
+    </S.FavButton>
+    <S.BuyBox>
+      <S.Price>{price}</S.Price>
+    </S.BuyBox>
+  </S.Wrapper>
+)
+
+export default GameCard
