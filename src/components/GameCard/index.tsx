4ac28220c12c2a7bf39ead5663eba4ab0c061800
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-10-28 08:48:38 -0300

    teste github

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -23,6 +23,7 @@ const GameCard = ({ title, developer, img, price }: GameCardProps) => (
     <S.BuyBox>
       <S.Price>{price}</S.Price>
     </S.BuyBox>
+    <div>teste</div>
   </S.Wrapper>
 )
 
